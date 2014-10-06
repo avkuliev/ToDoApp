bcf5182deb28033a13f79d77e4ca7c25667a8d32
Alexander Kuliev <avkuliev@gmail.com>
2014-10-06 20:32:06 +0400

    Initial Commit

---
+++ ToDoApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoApp
+//
+//  Created by Alexander Kuliev on 10/6/14.
+//  Copyright (c) 2014 Alexander Kuliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ToDoApp
+//
+//  Created by Alexander Kuliev on 10/6/14.
+//  Copyright (c) 2014 Alexander Kuliev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
